48d9f23a9489f847983e4a2f6843932f80d31205
Julien Hanon <julienhanon@MacBook-Pro-de-Julien.local>
2023-01-05 12:05:33 +0100

    ajout des dossier get et post

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+import zipfile
+
+
 
 class SayHello(App):
 	def build(self):
